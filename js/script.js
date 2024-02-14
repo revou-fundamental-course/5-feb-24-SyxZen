f34c12767a14edea3d27afd3f1cbfb966175527c
Syxzen <azistjahbedrek@gmail.com>
2024-02-14 12:45:07 +0700

    second setup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let correct = true;
 let resultValue = '';
 let inputValue = '';
+let caraCelcius = "90°C * (9/5) + 32 = 194°F";
+let caraFahrenheit = "194°F - 32 * (5/9) = 90°C";
 function convertCelcius(celcius) {
   return (celcius * 9/5) + 32;
 }
@@ -22,14 +24,19 @@ function doConvert(){
 document.getElementById("convert").addEventListener("click", function() {
   document.getElementById("result").value = resultValue;
 });
+document.getElementById("crk").value = caraCelcius;
 document.getElementById("reverse").addEventListener("click", function() {
   correct = !correct;
+  let inputPlaceholder = correct ? "Contoh : 90°C" : "Contoh : 194°F";
+  document.getElementById("input").placeholder = inputPlaceholder;
     if (correct) {
         document.getElementById("inputText").textContent = "C";
         document.getElementById("resultText").textContent = "F";
+        document.getElementById("crk").value = caraCelcius;
     } else {        
         document.getElementById("inputText").textContent = "F";
         document.getElementById("resultText").textContent = "C";
+        document.getElementById("crk").value = caraFahrenheit;
     }
     doConvert();
 });
